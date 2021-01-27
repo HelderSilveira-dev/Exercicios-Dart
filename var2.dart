859d296b355180f3d863e473db6747064395a546
Helder Silveira <helder.silveira.dev@gmail.com>
2021-01-27 19:58:41 -0300

    Dois Exercícios usando váriaveis.

---
+++ var2.dart
@@ -0,0 +1,25 @@
+main() {
+
+  var n1 = 25;
+  var n2 = 15;
+  var texto = ("O valor da subtração é: ");
+
+ print(texto + (n1-n2).toString());
+
+ var num1 = 2;
+ var num2 = 4.56;
+
+print(num1 is int);
+ print(num1+num2);
+
+ print(num1.runtimeType);
+ print(num2.runtimeType);
+
+var tex = ("Aló Dart!");
+print(tex);
+print(texto.runtimeType);
+print(tex is int);
+print(tex is String);
+
+  
+}
---
+++ variaveis1.dart
@@ -0,0 +1,9 @@
+main() {
+  int a = 18;
+  double b = 3.14;
+  int x = 2;
+  int y = 5;
+  double z = 1.86;
+
+  print((a+b)*3 + (y-x)*1000 / z);
+}
